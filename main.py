763f96425f5bb622cdaeae6ac179b95ef8c69d8c
AlexeyGold2077 <alexgold2077@gmail.com>
2024-01-20 13:19:07 +0300

    Some changes

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/items/{id}")
+def read_item(id: int, q: Union[str, None] = None):
+    return {"id": id, "parameter": q}
